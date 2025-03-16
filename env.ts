15b517c8cc14e9ae99425251f1ca56a193aac977
METR1CKA <ferchosalazar054@gmail.com>
2025-03-15 22:44:17 -0600

    feat (db): configuration added with sqlite

--- env.ts
+++ env.ts
@@ -15,10 +15,13 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
-	HOST: Env.schema.string({ format: 'host' }),
-	PORT: Env.schema.number(),
-	APP_KEY: Env.schema.string(),
-	APP_NAME: Env.schema.string(),
-  DRIVE_DISK: Env.schema.enum(['local'] as const),
-	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+    HOST: Env.schema.string({ format: 'host' }),
+    PORT: Env.schema.number(),
+    APP_KEY: Env.schema.string(),
+    APP_NAME: Env.schema.string(),
+    DRIVE_DISK: Env.schema.enum(['local'] as const),
+    NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+
+    // Database SQLite3 configuration only need this variable, others drivers and DBMS need more variables
+    DB_CONNECTION: Env.schema.string(),
 })
